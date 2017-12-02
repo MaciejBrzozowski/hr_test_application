352e6811480d189c3383df58d2a56a122ebae243
mbrzozowski <maciej.brzozowski@upaid.pl>
2017-12-02 13:23:15 +0100

    create basic security

---
+++ src/main/java/pl/brzozowski/maciej/clis/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package pl.brzozowski.maciej.clis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN", "USER", "ACTUATOR").and()
+                .withUser("user").password("user").roles("USER");
+    }
+}
---
+++ src/main/java/pl/brzozowski/maciej/clis/WebSecurity.java
@@ -0,0 +1,20 @@
+package pl.brzozowski.maciej.clis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/user/**").authenticated();
+    }
+}
