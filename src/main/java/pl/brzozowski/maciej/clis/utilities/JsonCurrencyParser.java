71eebd35dc7705fdf956a9e0445af2cae374168f
MaciejBrzozowski <maciej.brzozowski@upaid.pl>
2017-12-13 08:51:38 +0100

    small currency controller change
    Request base mapping at class

--- src/main/java/pl/brzozowski/maciej/clis/utilities/JsonCurrencyParser.java
+++ src/main/java/pl/brzozowski/maciej/clis/utilities/JsonCurrencyParser.java
@@ -5,18 +5,18 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
-import pl.brzozowski.maciej.clis.services.Curency;
+import pl.brzozowski.maciej.clis.services.Currency;
 
 import java.io.IOException;
 
 @Component
 @NoArgsConstructor
-public class JsonParser {
+public class JsonCurrencyParser {
 
-    public double jsonParserRequestOut(String response, Curency curency) throws IOException, ParseException {
+    public double jsonParserRequestOut(String response, Currency currency) throws IOException, ParseException {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject) jsonParser.parse(response);
         jsonObject = (JSONObject)jsonObject.get("rates");
-        return (Double) jsonObject.getOrDefault(curency.name().toUpperCase(), 0D);
+        return (Double) jsonObject.getOrDefault(currency.name().toUpperCase(), 0D);
     }
 }
